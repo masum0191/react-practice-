e79da9e0a98c82415e4ccc9997b4407fbfb2959d
iCircles <icircles@iCircless-MacBook-Pro.local>
2025-09-24 16:06:13 +0600

    functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
+    <div>
       <h1>welcome to react</h1>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
