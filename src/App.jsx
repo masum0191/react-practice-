2646f4a646611b10824360c49e4e6c3ed2ec44fa
iCircles <icircles@iCircless-MacBook-Pro.local>
2025-09-27 16:17:01 +0600

    input element

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 
 import { useRef } from 'react';
 const App = () => {
-let myHeadline=useRef();
+let firstName,lastName=useRef();
+
+
+
 const changehead=()=>{
-    myHeadline.current.src="https://picsum.photos/200/300";
-    myHeadline.current.style.border="2px solid red";
-    myHeadline.current.style.borderRadius="10px";
-    myHeadline.current.setAttribute('height','100');
-    myHeadline.current.setAttribute('width','100');
+
+   let fName=firstName.value;
+   let lName=lastName.value;
+   alert(`First Name is ${fName} and Last Name is ${lName}`);
+
+
   }
 
   return (
     <div>
-    <img src="https://picsum.photos/200/300" alt="Random" ref={myHeadline} />
+      <input type="text"  ref={(a)=>firstName=a} />
+      <input type="text"  ref={(a)=>lastName=a} />
     <button onClick={changehead}>Click Me</button>
     </div>
   );
