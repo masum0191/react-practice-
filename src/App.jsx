8f42cc4acdae64cbcd419a63525def27a7a89e8d
iCircles <icircles@iCircless-MacBook-Pro.local>
2025-09-27 12:43:35 +0600

    props function event pass

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import Hero from "./component/Hero";
 
 const App = () => {
-  const heroData={
-    name:"John Doe",
-    imgUrl:"https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
-    designation:"Software Engineer",
-    company:"Tech Solutions Inc.",
-    experience:5,
-    salary:75000,
-    email:"john.doe@example.com"
+  const ClickBtn=()=>{
+    alert("click me")
   }
   return (
     <div>
-      <Hero item={heroData} />
+      <Hero  childbtn={ClickBtn} />
     </div>
    
   );
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -2,15 +2,7 @@
 const Hero = (props) => {
     return (
         <div>
-            <ul>
-                <li>Name : {props.item.name}</li>
-                <li>Image : <img style={{width:"20px", height:"20px"}} src={props.item.imgUrl} alt={props.item.name} /></li>
-                <li>Designation : {props.item.designation}</li>
-                <li>Company : {props.item.company}</li>
-                <li>Experience : {props.item.experience} years</li>
-                <li>Salary : ${props.item.salary}</li>
-                <li>Email : {props.item.email}</li>
-            </ul>
+            <button onClick={props.childbtn}>Click Me</button>
         </div>
     );
 };
