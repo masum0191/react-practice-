f314e0a55a968d461c943cfb967c004394a5fd40
iCircles <icircles@iCircless-MacBook-Pro.local>
2025-09-27 12:26:55 +0600

    props object data pass

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import Hero from "./component/Hero";
 
 const App = () => {
+  const heroData={
+    name:"John Doe",
+    imgUrl:"https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+    designation:"Software Engineer",
+    company:"Tech Solutions Inc.",
+    experience:5,
+    salary:75000,
+    email:"john.doe@example.com"
+  }
   return (
     <div>
-     <Hero  title="Welcome to Our Website"  description="Your success starts here." />
+      <Hero item={heroData} />
     </div>
    
   );
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -2,8 +2,15 @@
 const Hero = (props) => {
     return (
         <div>
-            <h1>{props.title}</h1>
-            <p>{props.description}</p>
+            <ul>
+                <li>Name : {props.item.name}</li>
+                <li>Image : <img style={{width:"20px", height:"20px"}} src={props.item.imgUrl} alt={props.item.name} /></li>
+                <li>Designation : {props.item.designation}</li>
+                <li>Company : {props.item.company}</li>
+                <li>Experience : {props.item.experience} years</li>
+                <li>Salary : ${props.item.salary}</li>
+                <li>Email : {props.item.email}</li>
+            </ul>
         </div>
     );
 };
