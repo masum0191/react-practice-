387d3dc9f8d2fdffcb2d982f32783dd82603430c
iCircles <icircles@iCircless-MacBook-Pro.local>
2025-10-07 15:11:57 +0600

    todo list

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,44 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [myObject, setMyObject] = useState({ name: "John", age: 30 });
-  const updateObject = () => {
-    setMyObject((prevObject) => ({
-      ...prevObject,
-      name: 'Mary',
-      age: 25
-    }));
-  };
+ const [list,setlist]=useState([]);
+ const [item,setitem]=useState("");
+ const AddToList= ()=>{
+    list.push(item)
+    setlist([...list])
+ }
+ const removeItem=(index)=>{
+  list.splice(index,1)
+  setlist([...list])
+
+ }
 
   return (
     <div>
       <h1>Hello, World!</h1>
-      <p>Name: {myObject.name}</p>
-      <p>Age: {myObject.age}</p>
-      <button onClick={updateObject}>Update Object</button>
+      <table>
+        <tbody>
+          {
+            list.length!==0?(
+              list.map((item,index)=>{
+                return(
+                  <tr key={index}>
+                    <td>
+                      {item}
+                      <button onClick={()=>removeItem(index)} style={{marginLeft:"10px"}}>remove</button>
+                    </td>
+                  </tr>
+                )
+              })
+            ):(
+              <tr><td>No items</td></tr>
+            )
+          }
+        </tbody>
+      </table>
+      
+      <input type="text" onChange={(e)=>setitem(e.target.value)} placeholder='add item'/>
+      <button onClick={AddToList}>add</button>
     </div>
   );
 };
