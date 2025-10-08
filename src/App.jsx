5020cb4fb5476233491aaf1e4a5cc9b61cbab8f0
iCircles <icircles@iCircless-MacBook-Pro.local>
2025-10-08 09:35:18 +0600

    form submit object get useState

--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,35 @@
 import React, { useState } from 'react';
 
 const App = () => {
- const [list,setlist]=useState([]);
- const [item,setitem]=useState("");
- const AddToList= ()=>{
-    list.push(item)
-    setlist([...list])
- }
- const removeItem=(index)=>{
-  list.splice(index,1)
-  setlist([...list])
-
- }
+ let [FormObject ,SetFormObject] = useState({Fname:"",Lname:"",City:"",Gender:""})
+ const inputChange=(PropertyName,value)=>{
+    SetFormObject(preObject=>({
+      ...preObject,
+      [PropertyName]:value}))
+    
+  }
+  const FormSubmit=(e)=>{
+    e.preventDefault();
+    alert(JSON.stringify(FormObject));
+    console.log(FormObject);
+  }
 
   return (
-    <div>
-      <h1>Hello, World!</h1>
-      <table>
-        <tbody>
-          {
-            list.length!==0?(
-              list.map((item,index)=>{
-                return(
-                  <tr key={index}>
-                    <td>
-                      {item}
-                      <button onClick={()=>removeItem(index)} style={{marginLeft:"10px"}}>remove</button>
-                    </td>
-                  </tr>
-                )
-              })
-            ):(
-              <tr><td>No items</td></tr>
-            )
-          }
-        </tbody>
-      </table>
-      
-      <input type="text" onChange={(e)=>setitem(e.target.value)} placeholder='add item'/>
-      <button onClick={AddToList}>add</button>
+    <div className='container'>
+      <h1>Form</h1>
+     <form onSubmit={FormSubmit}>
+      <input onChange={(e)=>{inputChange("Fname",e.target.value)}} value={FormObject.Fname} type="text" placeholder='first name'/> <br />
+      <input onChange={(e)=>{inputChange("Lname",e.target.value)}} value={FormObject.Lname}  type="text" placeholder='last name'/> <br />
+      <select onChange={(e)=>{inputChange("City",e.target.value)}}
+      value={FormObject.City} >
+        <option value="">Select City</option>
+        <option value="Dhaka">Dhaka</option>
+        <option value="Rajshahi">Rajshahi</option>
+       </select><br />
+      <input onChange={()=>{inputChange("Gender",'male')}}  checked={FormObject.Gender==="male"} type="radio" name='gender' /> Male
+      <input onChange={()=>{inputChange("Gender",'female')}} checked={FormObject.Gender==="female"} type="radio" name='gender' /> Female <br />
+      <button type='submit'>submit</button>
+     </form>
     </div>
   );
 };
